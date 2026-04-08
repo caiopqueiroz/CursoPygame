9f7f44d75b50749f3a20cd0feb16bf9f4160e9e7
Caio Queiroz <caioq8421@gmail.com>
2026-04-08 06:30:06 -0300

    Aula 5

--- aula05/aula05.py
+++ aula05/aula05.py
@@ -7,7 +7,7 @@ pygame.init()
 tela = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Aula 5')
 clock = pygame.time.Clock()
-estado = 'jogando'
+estado = 'menu'
 # cores
 preto = (0, 0, 0)
 azul = (0, 0, 255)
@@ -18,6 +18,7 @@ l_player = 30
 h_player = 60
 r_player = l_player // 2
 v_player = 5
+pontos = 0 
 x_player = 100
 y_player = 100
 # inimigo
@@ -43,6 +44,12 @@ while True:
                 y_player = 100
                 x_inimigo = 700
                 y_inimigo = 500
+                x_item = 700
+                y_item = 100
+                pontos = 0
+                estado = 'jogando'
+        if evento.type == pygame.KEYDOWN:
+            if estado == 'menu' and evento.key == pygame.K_p:
                 estado = 'jogando'
 
     # lógica
@@ -100,17 +107,22 @@ while True:
 
     # desenho
     tela.fill(preto)
+    fonte = pygame.font.SysFont(None, 36)
     if estado == 'jogando':
         pygame.draw.rect(tela, azul, (x_player, y_player, l_player, h_player))
         pygame.draw.circle(tela, azul, (x_player + r_player, y_player - r_player), r_player)
         pygame.draw.rect(tela, vermelho, (x_inimigo, y_inimigo, l_inimigo, h_inimigo))
         pygame.draw.circle(tela, verde, (x_item, y_item), r_item)
     elif estado == 'game_over':
-        fonte = pygame.font.SysFont(None, 36)
-        #texto = fonte.render(f'Pontos: {pontos}', True, (255, 255, 255))
-        #tela.blit(texto, (10, 10))
+        texto = fonte.render(f'Pontos: {pontos}', True, (255, 255, 255))
+        tela.blit(texto, (10, 10))
         texto_game_over = fonte.render('GAME OVER', True, (255, 0, 0))
         tela.blit(texto_game_over, (325, 275))
+    elif estado == 'menu':
+        inicio = fonte.render('PRESSIONE P PARA JOGAR', True, (0, 0, 255))
+        tela.blit(inicio, (225, 275))
+        x_inimigo = 700
+        y_inimigo = 500
     
     # atualização
     pygame.display.update()
