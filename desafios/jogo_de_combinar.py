4ebd0f983f9dd4c61015e1fa004d077dbff1c342
Caio Queiroz <caioq8421@gmail.com>
2026-04-16 21:31:37 -0300

    Atualizações

--- desafios/jogo_de_combinar.py
+++ desafios/jogo_de_combinar.py
@@ -21,7 +21,11 @@ azul = (0, 0, 255)
 # estado
 estado = 'jogando'
 # peças 
-estado_peca = 'inicial'
+estado_peca = 'padrao'
+cor_peca_1 = vermelho
+cor_peca_2 = azul
+cor_peca_3 = azul
+cor_peca_4 = azul
 r = 30
 x1 = 800 // 2
 y1 = 600 // 2
@@ -47,7 +51,7 @@ while True:
     # lógica
     if estado == 'jogando':
         teclas = pygame.key.get_pressed()
-        
+
         # ativando seletor
         if teclas[pygame.K_x]:
             if cor_seletor == vermelho_transparente:
@@ -55,8 +59,47 @@ while True:
                 estado_seletor = True
             #if cor_seletor == verde_transparente:
             #   cor_seletor = vermelho_transparente
+        
+        # movendo o seletor desativado
+        
 
-        # movendo o seletor se ele estiver ativo
+        # definindo o estado da peça como 'trocar' caso a tecla seja pressionada
+        if teclas[pygame.K_w] and estado_seletor == True:
+            estado_peca = 'trocar'
+        if teclas[pygame.K_a] and estado_seletor == True:
+            estado_peca = 'trocar'
+        if teclas[pygame.K_s] and estado_seletor == True:
+            estado_peca = 'trocar'
+        if teclas[pygame.K_d] and estado_seletor == True:
+            estado_peca = 'trocar'
+            
+        # trocando a cor da primeira peça
+        if x_seletor + 5 + r == x1 and y_seletor + 5 + r == y1 and estado_peca == 'trocar':
+            cor_anterior = cor_peca_1
+            if cor_peca_1 == vermelho:
+                cor_peca_1 = azul
+            else:
+                cor_peca_1 = vermelho
+        if x_seletor + 5 + r == x2 and y_seletor + 5 + r == y2 and estado_peca == 'trocar':
+            cor_anterior = cor_peca_2
+            if cor_peca_2 == vermelho:
+                cor_peca_2 = azul
+            else:
+                cor_peca_2 = vermelho
+        if x_seletor + 5 + r == x3 and y_seletor + 5 + r == y3 and estado_peca == 'trocar':
+            cor_anterior = cor_peca_3
+            if cor_peca_3 == vermelho:
+                cor_peca_3 = azul
+            else:
+                cor_peca_3 = vermelho
+        if x_seletor + 5 + r == x4 and y_seletor + 5 + r == y4 and estado_peca == 'trocar':
+            cor_anterior = cor_peca_4
+            if cor_peca_4 == vermelho:
+                cor_peca_4 = azul
+            else:
+                cor_peca_4 = vermelho
+
+        # movendo o seletor 
         if estado_seletor == True and teclas[pygame.K_w]:
             y_seletor -= 70
             cor_seletor = vermelho_transparente
@@ -69,26 +112,33 @@ while True:
             y_seletor += 70
             cor_seletor = vermelho_transparente
             estado_seletor = False
-        if teclas[pygame.K_d]:
-            cor_peca = 'alterada'
-
-            if estado_seletor == True:
-                x_seletor += 70 
-                cor_seletor = vermelho_transparente
-                estado_seletor = False
+        if estado_seletor == True and teclas[pygame.K_d]:    
+            x_seletor += 70 
+            cor_seletor = vermelho_transparente
+            estado_seletor = False
+        
+        # fazendo a outra peça ganhar a cor anterior da peça trocada
+        if x_seletor + 5 + r == x1 and y_seletor + 5 + r == y1 and estado_peca == 'trocar':
+            cor_peca_1 = cor_anterior
+            estado_peca = 'padrao'
+        if x_seletor + 5 + r == x2 and y_seletor + 5 + r == y2 and estado_peca == 'trocar':
+            cor_peca_2 = cor_anterior
+            estado_peca = 'padrao'
+        if x_seletor + 5 + r == x3 and y_seletor + 5 + r == y3 and estado_peca == 'trocar':
+            cor_peca_3 = cor_anterior
+            estado_peca = 'padrao'
+        if x_seletor + 5 + r == x4 and y_seletor + 5 + r == y4 and estado_peca == 'trocar':
+            cor_peca_4 = cor_anterior
+            estado_peca = 'padrao'
 
     # desenho
     if estado == 'jogando':
         tela.fill(preto)
         
-        if x_seletor + 5 + r == x1 and y_seletor + 5 + r == y1 and cor_peca == 'alterada':
-            pygame.draw.circle(tela, azul, (x1, y1), r)
-        else:
-            pygame.draw.circle(tela, vermelho, (x1, y1), r)
-        
-        pygame.draw.circle(tela, azul, (x2, y2), r) # peça 2
-        pygame.draw.circle(tela, azul, (x3, y3), r) # peça 3
-        pygame.draw.circle(tela, azul, (x4, y4), r) # peça 4
+        pygame.draw.circle(tela, cor_peca_1, (x1, y1), r) # peça 1 
+        pygame.draw.circle(tela, cor_peca_2, (x2, y2), r) # peça 2
+        pygame.draw.circle(tela, cor_peca_3, (x3, y3), r) # peça 3
+        pygame.draw.circle(tela, cor_peca_4, (x4, y4), r) # peça 4
         
         # seletor 
         seletor = pygame.Surface((70, 70), pygame.SRCALPHA)
