429b738f4a97b2ada61a74e98f772d9c64d80be2
Caio Queiroz <caioq8421@gmail.com>
2026-05-14 20:20:06 -0300

    Atualizações

--- aura/cena1.py
+++ aura/cena1.py
@@ -1,4 +1,6 @@
-# personagem acordando no quarto 
+# jogo simples de loop que se passa em um único cenário - a sala de aula
+
+# a professora te faz uma pergunta: o que significa a sigla PIB? 
 
 
 import pygame
@@ -12,13 +14,27 @@ clock = pygame.time.Clock()
 # cores 
 preto = (0, 0, 0)
 branco = (255, 255, 255)
+cinza = (211, 211, 211)
+marrom = (105, 38, 14)
+azul = (68, 93, 194)
+verde_escuro = (9, 41, 5)
 # estados
-estado = 'inicio'
+estado = 'jogando'
 # fonte
 titulo = pygame.font.SysFont(None, 72)
 texto = pygame.font.SysFont(None, 36)
-# texto 
-frase = ''
+
+# professora
+l_professora = 30
+h_professora = 90
+x_professora = 500
+y_professora = 100
+
+# alunos
+l_alunos = 30
+h_alunos = 30
+x_alunos = 75
+y_alunos = 300
 
 while True:
     # eventos 
@@ -33,14 +49,22 @@ while True:
 
     # desenho
     if estado == 'inicio':
-        # exibindo tela inicial 
-        tela.fill(preto)
-        frase = 'Farming Aura Simulator'
-        mensagem = titulo.render(frase, True, branco)
-        tela.blit(mensagem, (100, 100))
-        frase = 'Aperte P para começar'
-        mensagem = texto.render(frase, True, branco)
-        tela.blit(mensagem, (100, 200))
+        pass
+    if estado == 'jogando':
+        # Adicionando cor ao chão da sala de aula 
+        tela.fill(cinza)
+        # Quadro
+        pygame.draw.rect(tela, verde_escuro, (250, 10, 300, 100))
+        # Professora
+        pygame.draw.rect(tela, marrom, (x_professora, y_professora, l_professora, h_professora))
+        # Alunos 
+        for posicao_vertical in range(0, 3):
+            for posicao_horizontal in range(0, 5):
+                pygame.draw.rect(tela, azul, (x_alunos + 150 * posicao_horizontal, y_alunos + 100 * posicao_vertical, l_alunos, h_alunos))
+        # Exibindo pergunta da professora
+        frase = 'O que significa a sigla PIB?'
+        mensagem = texto.render(frase, True, preto)
+        tela.blit(mensagem, (100, 150))
 
     # atualizações
     pygame.display.update()
